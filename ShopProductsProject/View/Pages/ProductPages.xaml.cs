4c88568918de1d640feb3b67dda420613737d870
Сын <iamvladgubarevofficial@gmail.com>
2022-12-12 09:17:37 +0500

    Сделана вёрстка шапки

--- ShopProductsProject/View/Pages/ProductPages.xaml.cs
+++ ShopProductsProject/View/Pages/ProductPages.xaml.cs
@@ -1,5 +1,4 @@
-﻿using ShopProductsProject.View.Pages;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,17 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ShopProductsProject
+namespace ShopProductsProject.View.Pages
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для ProductPages.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ProductPages : Page
     {
-        public MainWindow()
+        public ProductPages()
         {
             InitializeComponent();
-            MainFrame.Navigate(new ProductPages());
         }
     }
 }
